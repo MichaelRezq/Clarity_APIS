bea2b75df638bd7061c4515c079dd7e80c8535f2
Michael Rezq <michaelrezq9@gmail.com>
2023-03-20 06:51:23 +0200

    update in admin register

--- clarity/problems/admin.py
+++ clarity/problems/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Problem
 
 # Register your models here.
+
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    list_filter = ('created_at', 'updated_at', 'author')
+
+admin.site.register(Problem, ProblemAdmin)
