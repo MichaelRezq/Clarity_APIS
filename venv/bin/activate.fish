8b9c2f1b9dc20723df014be1fd7d89b402d92690
youssef <eng.youssef2023@gmail.com>
2023-04-01 04:03:29 +0200

    add new problem

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/media/michrezq/MICHAEL/Clarity_APIS/venv"
+set -gx VIRTUAL_ENV "/home/youssef/final-project/Clarity_APIS/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
