b2236d74e8f4c798a4cdf649d31ab5a62e79fba0
youssef <eng.youssef2023@gmail.com>
2023-04-01 04:01:55 +0200

    add problems

--- clarity/clarity/settings.py
+++ clarity/clarity/settings.py
@@ -186,14 +186,25 @@ REST_FRAMEWORK = {
     'rest_framework.authentication.TokenAuthentication',
     ],
  }
+
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'clarity',
+            'USER': 'postgres',
+            'PASSWORD': '1512000',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
 }
 # DATABASES = {
 #     'default': {
--- clarity/problems/api/urls.py
+++ clarity/problems/api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import ProblemListCreateView, ProblemRetrieveUpdateDeleteView
+from .views import get_post_problems,get_edit_delete_problem,get_post_solution,get_edit_delete_solution,LikeView\
+    # ,LikeListCreateView,LikeRetrieveUpdateDeleteView
 
 urlpatterns = [
-    path('problems/', ProblemListCreateView.as_view(), name='problem_list_create'),
-    path('problems/<int:pk>/', ProblemRetrieveUpdateDeleteView.as_view(), name='problem_retrieve_update_delete'),
+    path('problems/', get_post_problems, name='problem_list_create'),
+    path('problems/<int:pk>/', get_edit_delete_problem, name='problem_retrieve_update_delete'),
+    path('solution/<int:problemID>/', get_post_solution, name='solutions'),
+    path('solution/<int:problemID>/<int:solutionID>', get_edit_delete_solution, name='solutions_retrieve_update_delete'),
+    path('solution/<int:post_id>/like', LikeView.as_view(), name='like_view'),
+    # path('solution/<int:solution_id>/help', LikeRetrieveUpdateDeleteView.as_view(), name='like_view'),
+
 ]
